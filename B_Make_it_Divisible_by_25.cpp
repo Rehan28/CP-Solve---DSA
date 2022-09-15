420c8fe9c8b141fc1b866344f69859bd1e4e42b0
Rehan28 <71427882+Rehan28@users.noreply.github.com>
2022-09-16 00:59:55 +0600

    9/16/2022

---
+++ B_Make_it_Divisible_by_25.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+//Muku28
+using namespace std;typedef long long ll;
+void Muku28(){ios_base::sync_with_stdio(false);cin.tie(NULL);}
+void printi(int x){cout<<x<<"\n";}void printl(ll x){cout<<x<<"\n";}
+void prints(string x){cout<<x<<"\n";}
+void show(int a[], int arraysize){for (int i = 0; i < arraysize; ++i){
+cout << a[i] << ",";}}void dbg(int x) {cout << "x is " << x << endl; }
+typedef vector<int> vi;typedef vector<string> vs;typedef pair<int, int> pii;
+#define cinv(v,n) for(int i=0;i<n;i++){int a;cin>>a;v.push_back(a);}
+#define cin(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(v) v.begin(),v.end()
+#define nl cout<<"\n";
+//Muku28
+int main()
+{
+    Muku28();
+     int t;
+    cin>>t;
+   while(t--)
+    {
+        string s;
+        cin>>s;
+        int c = 0;
+        for(int i=s.size()-1;i>0;i--)
+        {
+            if((s[i] =='0') and (s[i-1] =='0' ) || (s[i] =='2') and (s[i-1] =='5'))
+            {
+                c = s.size()  - i;
+                break;
+            }
+            else if((s[i] =='5' ) and (s[i-1] =='0') || (s[i] =='7') and (s[i-1] =='5'))
+            {
+                 c = s.size()  - i;
+                 break;
+            }
+        }
+        printi(c);
+    }
+  return 0;
+ }
