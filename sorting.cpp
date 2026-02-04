35b49836cb19e090ac367f22adfa80c20e97f270
Rakibul Islam Rehan <rakibulislamrehan@gmail.com>
2026-02-04 23:43:17 +0600

    Now

--- sorting.cpp
+++ sorting.cpp
@@ -220,3 +220,27 @@ for merge sort just when add two sorted array then count inversions
 
 
 
+#include <bits/stdc++.h>
+using namespace std;
+void Muku28(){ios_base::sync_with_stdio(false);cin.tie(NULL);}
+#define cin(a,n) for(int i=0;i<n;i++){ cin>>a[i];}
+#define nl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<nl;
+#define int long long
+
+
+void solve(int test) {
+  int n;
+  cin>>n;
+  int a[n];
+  cin(a,n);
+}
+int32_t main() {
+  Muku28();
+  int test=1;
+  cin>>test;
+  for(int i=1;i<=test;i++) {
+    solve(i);
+  }
+  return 0;
+}
